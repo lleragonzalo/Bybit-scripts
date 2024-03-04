75ccbdb9ae367459603b1a14c1467cd762d56dd0
Gonzalo Llera <66426395+lleragonzalo@users.noreply.github.com>
2024-03-04 14:29:24 -0300

    Update borrowingYearToExcel.py

--- borrowingYearToExcel.py
+++ borrowingYearToExcel.py
@@ -16,7 +16,6 @@ def obtener_datos_borrowing(symbol):
         coin = symbol[:-4]  # Extraer la moneda del símbolo (por ejemplo, "SOL" de "SOLUSDT")
 =======
         coin = symbol[:-4] 
->>>>>>> 9cf4443d7e0e107ef1a9c46a6f3a6f352a73f1fc
         timestamp = str(int(time.time() * 1000))
         query_params = f'timestamp={timestamp}&api_key={API_KEY}&coin={coin}'
         message = f'GET{endpoint}?{query_params}'
@@ -62,10 +61,6 @@ def main():
                'BICO', 'QNT', 'MNT', 'LUNC', 'APT', 'MASK', 'AR', 'SOL', 'TWT', 'CORE', 'BLUR', 'HFT', 'MAGIC', 'LDO',
                'ARB', 'SUI', 'PEPE', 'AGIX', 'RNDR', 'WLD', 'SEI', 'CYBER', 'ARKM', 'HBAR', 'INJ']
 
-<<<<<<< HEAD
-    # Lista para almacenar DataFrames de cada moneda
-=======
-  
     dataframes = []
 
     for symbol in monedas:
